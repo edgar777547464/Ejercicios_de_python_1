5e339e9f41a6433439867ff505fc900a58097977
Edgar <edigk390@gmail.com>
2026-02-07 20:58:24 -0600

    subiendo ejercicios finales

--- condicionales/ejercicio1.py
+++ condicionales/ejercicio1.py
@@ -7,7 +7,7 @@ n1 = int(input("Coloca un numero: "))
 n2 = int(input("Coloca otro numero: "))
 
 if n1 > n2:
-	print("El ",n1" es mas grande que el ",n2)
+print("El ",n1" es mas grande que el ",n2)
 
 else:
 	print(f:"Número {n1} NO es mayor que número {n2}")

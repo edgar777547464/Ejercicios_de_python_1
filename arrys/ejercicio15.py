f7524fe3bbd8e1bee6ce7eb3f508652206d988a6
Edgar <edigk390@gmail.com>
2026-03-23 19:58:02 -0600

    agregando ultimos ejercicios

--- arrys/ejercicio15.py
+++ arrys/ejercicio15.py
@@ -11,3 +11,23 @@
 #//El programa ira pidiendo los nombres de los equipos de cada partido y el 
 #//resultado del partido, a continuación se imprimirá la quiniela de esa jornada.
 
+num_equipos = 15
+equipos = [["",""] for _ in range(num_equipos)]
+resultados = [[0,0] for _ in range(num_equipos)]
+
+for indice in range(num_equipos):
+    equipos[indice][0] = input(f"Introduce el nombre del equipo 1 del partido {indice+1}: ")
+    equipos[indice][1] = input(f"Introduce el nombre del equipo 2 del partido {indice+1}: ")
+    resultados[indice][0] = int(input(f"Introduce los goles metidos por el equipo {equipos[indice][0]}: "))
+    resultados[indice][1] = int(input(f"Introduce los goles metidos por el equipo {equipos[indice][1]}: "))
+
+print("QUINIELA")
+print("========")
+
+for indice in range(num_equipos):
+    if resultados[indice][0] > resultados[indice][1]:
+        print(f"{equipos[indice][0]} - {equipos[indice][1]} -> 1")
+    elif resultados[indice][0] < resultados[indice][1]:
+        print(f"{equipos[indice][0]} - {equipos[indice][1]} -> 2")
+    else:
+        print(f"{equipos[indice][0]} - {equipos[indice][1]} -> X")
